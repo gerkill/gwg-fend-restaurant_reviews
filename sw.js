832cb721732c0e5a73096e55d2e57d03171b157d
Ger Killeen <35434414+gerkill@users.noreply.github.com>
2018-08-29 16:34:58 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,65 +1,73 @@
-const cacheName = 'restaurant_reviews';
-
-const filesToCache = [
-  '/index.html',
-  '/restaurant.html',
-  '/data/restaurants.json',
-  '/img/1.jpg',
-  '/img/2.jpg',
-  '/img/3.jpg',
-  '/img/4.jpg',
-  '/img/5.jpg',
-  '/img/6.jpg',
-  '/img/7.jpg',
-  '/img/8.jpg',
-  '/img/9.jpg',
-  '/img/10.jpg',
-  '/css/styles.css',
-  '/js/dbhelper.js',
-  '/js/main.js',
-  '/js/restaurant_info.js',
-];
+const staticCache = 'restaurant-reviews';
 
 /**
  * Creates cache when the Service Worker installs
  */
-self.addEventListener('install', event => {
-  console.log('[ServiceWorker] Install');
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log('[ServiceWorker] Caching app');
-      return cache.addAll(filesToCache);
-    }),
-  );
-});
 
-/**
- *  Purges the previous cache after activating the next cache
- */
-self.addEventListener('activate', event => {
-  console.log('[ServiceWorker] Activate');
-  event.waitUntil(
-    caches.keys().then(keyList =>
-      Promise.all(
-        keyList.map(key => {
-          if (key !== cacheName) {
-            return caches.delete(key);
-          }
-        }),
-      ),
-    ),
-  );
+self.addEventListener('install', event => {
+    console.log('Service Worker install');
+    event.waitUntil(
+        caches.open(staticCache)
+        .then(cache => {
+            console.log('caching')
+            return cache.addAll([
+                '/',
+                '/favicon.ico',
+                '/index.html',
+                '/restaurant.html',
+                '/data/restaurants.json',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+                '/css/styles.css',
+                '/js/dbhelper.js',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+            ]).catch(error => {
+                console.log('Caches open failed: ' + error);
+            });
+        })
+    );
 });
 
 /**
  * Responds with cached content if matched
  */
-self.addEventListener('fetch', event => {
-  const request = event.request.url.includes('/restaurant.html')
-    ? new Request('/restaurant.html')
-    : event.request;
 
-  event.respondWith(
-    caches.match(request).then(response => response || fetch(request)),
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).then(fetchResponse => {
+                return caches.open(staticCache).then(cache => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            });
+        })
+    );
+});
+
+/**
+ *  Purges the previous cache after activating the next cache
+ */
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => {
+                    return cacheName.startsWith('restaurant-reviews-') && cacheName !== staticCacheName;
+                }).map(cacheName => {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
 });
